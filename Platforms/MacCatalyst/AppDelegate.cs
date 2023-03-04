a7b6675034ac90b9d5b7348474665c73c4f95279
romelbarreto <romelbarreto@gmail.com>
2023-03-04 18:20:15 -0300

    Initial commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VL;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
